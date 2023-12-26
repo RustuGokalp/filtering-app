2c4c3055c962c71d83119ca76efdd796b2b3bf80
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2023-12-26 14:35:07 +0300

    Integrate axios into project and send get request

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import SearchHeader from "./SearchHeader";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import searchImages from "./api";
 function App() {
+  const handleSubmit = (term) => {
+    searchImages(term);
+  };
   return (
     <div>
-      <SearchHeader />
+      <SearchHeader search={handleSubmit} />
     </div>
   );
 }
--- src/SearchHeader.jsx
+++ src/SearchHeader.jsx
@@ -1,13 +1,18 @@
 import "./SearchHeader.css";
 
-function SearchHeader() {
+function SearchHeader({ search }) {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    search("Gökalp");
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-12 my-3">
-          <form className="formWrapper">
-            <label className="mb-3">Ne Arıyorsunuz?</label>
-            <input className="form-control" placeholder="Default input" />
+          <form className="formWrapper" onSubmit={handleFormSubmit}>
+            <label className="mb-3">What are you looking for?</label>
+            <input className="form-control" placeholder="Placeholder" />
           </form>
         </div>
       </div>
